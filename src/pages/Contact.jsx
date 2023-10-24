ba19cc1f438f9a3c6a4ad14b5c9c21ca1aec3916
Jolo <bobtangpuz@gmail.com>
2023-10-25 00:46:15 +0800

    skladfj

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import contactbanner from '../assets/contactbanner.jpg'
+import Footer from '../components/Footer'
 
 const Contact = () => {
 
@@ -25,8 +26,51 @@ const Contact = () => {
         </div>
       </div>
 
-     
-
+      <div className='w-full bg-gray-50'>
+        <div className='mx-auto max-w-[900px]'>
+        <h2 className='text-2xl font-semibold mb-4'>Contact Us</h2>
+        <form>
+          <div className='mb-4 grid grid-cols-2 gap-8'>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              className='w-full px-3 py-2 border rounded-md'
+              required
+            />
+            <input
+              type='email'
+              id='email'
+              name='email'
+              
+              className='w-full px-3 py-2 border rounded-md'
+              required
+            />
+          </div>
+          <div className='mb-4'>
+            <label htmlFor='message' className='block text-sm font-medium'>
+              Message
+            </label>
+            <textarea
+              id='message'
+              name='message'
+           
+              className='w-full px-3 py-2 border rounded-md'
+              required
+            />
+          </div>
+          <div className='flex justify-center'>
+          <button
+            type='submit'
+            className='bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600'
+          >
+            Submit
+          </button>
+          </div>
+        </form>
+      </div>
+      </div>
+      <Footer />
     </>
   )
 }
