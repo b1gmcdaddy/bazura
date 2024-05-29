daea72b7376618abe2627842316c8ff840d7616a
b1gmcdaddy <bobtangpuz@gmail.com>
2024-05-29 20:35:05 +0800

    cors config

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 dotenv.config();
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://bazura.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }));
